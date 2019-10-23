8c9d686bbe3b7dfdc4f660eb752f99de617a52d7
Edoardo Debenedetti <edoardo.debenedetti@epfl.ch>
2019-10-23 21:18:20 +0000

    Implement logistic SGD and Newton

--- src/logistic/not_req_impl.py
+++ src/logistic/not_req_impl.py
@@ -1,11 +1,12 @@
 import numpy as np
 from typing import Tuple
+from random import randrange
 
-from src.logistic.cost import compute_loss
+from src.logistic.loss import compute_loss
 from src.logistic.gradient import compute_gradient
 from src.logistic.hessian import compute_hessian
 
-def reg_logistic_regression(y: np.ndarray, tx: np.ndarray, lambda_: float, initial_w: np.ndarray,
+def reg_logistic_regression(y: np.ndarray, tx: np.ndarray, initial_w: np.ndarray, lambda_: float,
                             max_iters: int, gamma: float, method: str ='sgd') -> Tuple[np.ndarray, float]:
     """
     Does the regularized logistic regression.
@@ -42,7 +43,7 @@ def reg_logistic_regression(y: np.ndarray, tx: np.ndarray, lambda_: float, initi
         The loss given w as parameters.
     """
     
-    assert method is in ['sgd', 'newton', 'gd'] "method should be either 'sgd', 'newton' or 'gd'"
+    assert method in ['sgd', 'newton', 'gd'], "Argument 'method' must be either " + ", ".join(f"'{x}'" for x in method)
 
     # init parameters
     threshold = 1e-8
@@ -52,10 +53,11 @@ def reg_logistic_regression(y: np.ndarray, tx: np.ndarray, lambda_: float, initi
     # start the logistic regression
     for iter in range(max_iters):
         # get loss and update w.
-        loss, w = gradient_descent_step(y, tx, w, gamma, lambda_, method=method)
+        loss, w, gradient = gradient_descent_step(y, tx, w, gamma, lambda_, method=method)
         # log info
         if iter % 100 == 0:
             print("Current iteration={i}, loss={l}".format(i=iter, l=loss))
+            print("||d|| = {d}".format(d=np.linalg.norm(gradient)))
         # converge criterion
         losses.append(loss)
         if len(losses) > 1 and np.abs(losses[-1] - losses[-2]) < threshold:
@@ -97,7 +99,7 @@ def logistic_regression(y: np.ndarray, tx: np.ndarray, initial_w: np.ndarray,
         The loss given w as parameters.
     """
     
-    assert method is in ['sgd', 'newton', 'gd'] "method should be either 'sgd', 'newton' or 'gd'"
+    assert method in ['sgd', 'newton', 'gd'], "Argument 'method' must be either " + ", ".join(f"'{x}'" for x in method)
 
     return reg_logistic_regression(y, tx, 0, initial_w, max_iters, gamma, method=method)
 
@@ -129,18 +131,33 @@ def gradient_descent_step(y: np.ndarray, tx: np.ndarray, w: np.ndarray, gamma:np
         The loss given w as parameters.
     """
     
-    assert method is in ['sgd', 'newton', 'gd'] "method should be either 'sgd', 'newton' or 'gd'"
+    assert method in ['sgd', 'newton', 'gd'], "Argument 'method' must be either " + ", ".join(f"'{x}'" for x in method)
     
     # TODO: implement different methods
     
     # Get loss, gradient, hessian
     loss = compute_loss(y, tx, w, lambda_=lambda_)
-    gradient = compute_gradient(y, tx, w, lambda_=lambda_)
-    hessian = compute_hessian(y, tx, w, lambda_=lambda_)
+    
+    
+    rand_idx = randrange(y.shape[0])
+    rand_tx = tx[rand_idx, :].reshape((-1, 1)).T
+    rand_y = y[rand_idx]
+    
+    # Get the gradient of all the points (GD), or in a stochastic way (SGD, Newton)
+    # We use this approach for Newton method to save memory.
+    gradient = (compute_gradient(y, tx, w, lambda_=lambda_) if method == 'gd'
+                else compute_gradient(rand_y, rand_tx, w, lambda_=lambda_))
+    # Default case: w = w - gamma * g where g is the gradient
+    g = gradient
+    
+    # In case the method is newton we need to compute the hessian
+    if method == 'newton':
+        hessian = compute_hessian(rand_y, rand_tx, w, lambda_=lambda_)
+        g = (np.linalg.inv(hessian)).dot(gradient)
 
     # Update w
-    w = w - gamma * (np.linalg.inv(hessian)).dot(gradient)
+    w = w - gamma * g
     
-    return loss, w
+    return loss, w, gradient
 
     
