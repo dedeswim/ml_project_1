4d6f8b9645d53fb4b2096057f5073d4e6fa99bab
DSAureli <ds.aureli@gmail.com>
2019-10-24 09:34:51 +0200

    Add function for polynomial matrix generation via Vandermonde matrix

--- src/regression/polynomials.py
+++ src/regression/polynomials.py
@@ -3,6 +3,8 @@ import numpy as np
 def build_poly_matrix(x, degree):
     """polynomial basis functions for input data x, for j=0 up to j=degree."""
 
+    # TOFIX
+
     repetitions = np.ones(len(x.shape) + 1, dtype=int)
     repetitions[0] = degree + 1
 
@@ -15,3 +17,12 @@ def build_poly(x, degree):
     """polynomial basis functions for input data x, for j=0 up to j=degree."""
 
     return np.power(x.repeat(degree + 1).reshape(x.shape[0], -1), np.arange(degree + 1))
+
+
+def build_poly_matrix_vandermonde(x, degree):
+    """
+    TOWRITE
+    """
+
+    mat_van = np.polynomial.polynomial.polyvander(x.T, degree)
+    return np.hstack(list(mat_van[x] for x in range(mat_van.shape[0])))
